a2b69c8fe2c405c16b77200639c1ea657bb0bd36
Philipe Farias <philipefarias@gmail.com>
2015-06-04 23:34:54 -0300

    Clearer jsons in ruby
    Just a more clearer way for writing jsons in ruby.

--- spec/api/response_spec.rb
+++ spec/api/response_spec.rb
@@ -7,7 +7,7 @@ describe FyberOffers::API::Response do
   end
 
   let :json_response do
-    "{ \"code\" : \"OK\" }"
+    %{{ "code" : "OK" }}
   end
 
   def new_response(json)
@@ -15,75 +15,87 @@ describe FyberOffers::API::Response do
   end
 
   describe "parsing response" do
+    it "raises exception for blank body" do
+      proc {
+        new_response %{{ }}
+      }.must_raise FyberOffers::API::Error::InvalidResponse
+    end
+
+    it "raises exception for blank code" do
+      proc {
+        new_response %{{ "code" : "" }}
+      }.must_raise FyberOffers::API::Error::InvalidResponse
+    end
+
     it "raises exception for invalid page error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_PAGE\" }"
+        new_response %{{ "code" : "ERROR_INVALID_PAGE" }}
       }.must_raise FyberOffers::API::Error::InvalidPage
     end
 
     it "raises exception for invalid appid error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_APPID\" }"
+        new_response %{{ "code" : "ERROR_INVALID_APPID" }}
       }.must_raise FyberOffers::API::Error::InvalidAppid
     end
 
     it "raises exception for invalid uid error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_UID\" }"
+        new_response %{{ "code" : "ERROR_INVALID_UID" }}
       }.must_raise FyberOffers::API::Error::InvalidUid
     end
 
     it "raises exception for invalid hashkey error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_HASHKEY\" }"
+        new_response %{{ "code" : "ERROR_INVALID_HASHKEY" }}
       }.must_raise FyberOffers::API::Error::InvalidHashkey
     end
 
     it "raises exception for invalid device_id error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_DEVICE_ID\" }"
+        new_response %{{ "code" : "ERROR_INVALID_DEVICE_ID" }}
       }.must_raise FyberOffers::API::Error::InvalidDeviceId
     end
 
     it "raises exception for invalid ip error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_IP\" }"
+        new_response %{{ "code" : "ERROR_INVALID_IP" }}
       }.must_raise FyberOffers::API::Error::InvalidIp
     end
 
     it "raises exception for invalid timestamp error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_TIMESTAMP\" }"
+        new_response %{{ "code" : "ERROR_INVALID_TIMESTAMP" }}
       }.must_raise FyberOffers::API::Error::InvalidTimestamp
     end
 
     it "raises exception for invalid locale error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_LOCALE\" }"
+        new_response %{{ "code" : "ERROR_INVALID_LOCALE" }}
       }.must_raise FyberOffers::API::Error::InvalidLocale
     end
 
     it "raises exception for invalid android_id error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_ANDROID_ID\" }"
+        new_response %{{ "code" : "ERROR_INVALID_ANDROID_ID" }}
       }.must_raise FyberOffers::API::Error::InvalidAndroidId
     end
 
     it "raises exception for invalid category error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INVALID_CATEGORY\" }"
+        new_response %{{ "code" : "ERROR_INVALID_CATEGORY" }}
       }.must_raise FyberOffers::API::Error::InvalidCategory
     end
 
     it "raises exception for server error" do
       proc {
-        new_response "{ \"code\" : \"ERROR_INTERNAL_SERVER_ERROR\" }"
+        new_response %{{ "code" : "ERROR_INTERNAL_SERVER_ERROR" }}
       }.must_raise FyberOffers::API::Error::RemoteServerError
     end
 
     it "raises exception for unknown code" do
       proc {
-        new_response "{ \"code\" : \"SOME_RANDOM_CODE\" }"
+        new_response %{{ "code" : "SOME_RANDOM_CODE" }}
       }.must_raise FyberOffers::API::Error::UnknownCode
     end
 
