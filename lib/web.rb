29c9f9b3e964ab8cdb7bbf4bbdf690262d7fe64a
Philipe Farias <philipefarias@gmail.com>
2015-06-06 21:07:34 -0300

    Removing Web::Repo
    Don't need it.
    
    Web#post will render the results directly.

--- lib/web.rb
+++ lib/web.rb
@@ -2,14 +2,12 @@ require_relative "api"
 require_relative "web/offers_form"
 require_relative "web/fetcher"
 require_relative "web/router"
-require_relative "web/repo"
 require "yaml"
 
 module FyberOffers
   module Web
     CONFIG_FILE_PATH = File.expand_path("../../config.yml", __FILE__)
     CONFIG = YAML.load File.read(CONFIG_FILE_PATH)
-    REPO = Repo.new
 
     def app
       Router.new
